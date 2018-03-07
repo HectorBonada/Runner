9d6014517787f992e85c2024ffea9851bf0fdb29
Albert Rodriguez <albertrodriespada@gmail.com>
2018-03-07 18:23:06 +0100

    grammar issue

--- Assets/Scripts/Gameplay/ScoreManager.cs
+++ Assets/Scripts/Gameplay/ScoreManager.cs
@@ -38,7 +38,7 @@ public class ScoreManager : MonoBehaviour {
             PlayerPrefs.SetFloat("HighScore", highScoreCount);
         }
 
-        scoreText.text = "Score: " + Mathf.Round( scoreCount);
+        scoreText.text = "Scpre: " + Mathf.Round( scoreCount);
         highScoreText.text = "High Score: " + Mathf.Round(highScoreCount);
 
 	}
