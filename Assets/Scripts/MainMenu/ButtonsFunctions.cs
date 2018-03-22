dd4f3c254ca196aa02e30459a748533101167ba5
- <-@gmail.com>
2018-03-22 18:38:24 +0100

    sounds added

--- Assets/Scripts/MainMenu/ButtonsFunctions.cs
+++ Assets/Scripts/MainMenu/ButtonsFunctions.cs
@@ -8,6 +8,7 @@ public class ButtonsFunctions : MonoBehaviour
     public GameObject managerScene;
     private LevelManager script;
     public GameManager gameManager;
+    public AudioSource sounds;
 
     private void Start()
     {
@@ -24,21 +25,30 @@ public class ButtonsFunctions : MonoBehaviour
 
     public void NewGame()
     {
+        sounds.Play();
         script.LoadNext();
     }
 
     public void TryAgain()
     {
+        sounds.Play();
         gameManager.RestartGame();
     }
 
     public void MainGame()
     {
+        sounds.Play();
         script.LoadMenu();
     }
 
     public void QuitGame()
     {
+        sounds.Play();
         script.ExitGame();
+    }
+
+    public void SoundClick()
+    {
+        sounds.Play();
     }
 }
