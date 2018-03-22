505cf078606b3a5251c19895e1885c422ef20787
albert <albertrodriespada@gmail.com>
2018-03-22 16:21:21 +0100

    platformsfalling

---
+++ Assets/Scripts/Gameplay/Hazard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour {
+
+	public PlayerBehaviour player;
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerBehaviour> ();
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Debug.Log ("PlayerDebeMorir");
+			player.isDead = true;
+		}
+
+	}
+}
